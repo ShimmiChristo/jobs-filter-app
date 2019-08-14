58138cd7eb403f0acedbf903056df127407c2dcb
shimmiChristo <shimmin.christopher@gmail.com>
2019-08-13 23:30:06 -0400

    displaying the date, title, and description

--- client/src/components/CraigslistJobs.js
+++ client/src/components/CraigslistJobs.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Suggestions = (props) => {
   const options = props.jobs.map(r => (
-    <li key={r.date}>
-      {r.title}
-    </li>
+    <div className='cl-listing' key={r.date}>
+      <div className='cl-title'>{r.title}</div>  <br></br> <div className='cl-date'>{r.date}</div>  <br></br> <div className="cl-description">{r.description}</div> 
+    </div>
   ))
-  return <ul>{options}</ul>
+  return <div className="cl-jobs">{options}</div>
 }
 
 export default Suggestions
